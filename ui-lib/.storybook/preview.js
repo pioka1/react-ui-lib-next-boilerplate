b78d5ddc561bdfe991da6b7143058dec40676486
Niclas Horstad <niclas.horstad@gmail.com>
2022-03-24 23:04:32 +0100

    svg and theme storybook

--- ui-lib/.storybook/preview.js
+++ ui-lib/.storybook/preview.js
@@ -1,3 +1,26 @@
+import { addDecorator } from "@storybook/react";
+import { withThemesProvider } from "storybook-addon-emotion-theme";
+
+const theme1 = {
+  name: "ECorp Test Theme 1",
+  color: {
+    primary: "red",
+    secondary: "#555",
+    tertiary: "#777"
+  }
+};
+const theme2 = {
+  name: "ECorp Test Theme 2",
+  color: {
+    primary: "blue",
+    secondary: "#555",
+    tertiary: "#777"
+  }
+};
+
+const themes = [theme1, theme2];
+addDecorator(withThemesProvider(themes));
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
--- ui-lib/src/atoms/Button/Button.tsx
+++ ui-lib/src/atoms/Button/Button.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 
 import { StyledButton } from "./Button.styles";
+import { Drink } from "../Icon/generated";
 
 export const Button:React.FC = ({ children }) => {
-	return <StyledButton>{children}</StyledButton>;
+	return (
+		<StyledButton>
+			{children}
+			<Drink height={50} width={50} />
+		</StyledButton>
+	);
 };
